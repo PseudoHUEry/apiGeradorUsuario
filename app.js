948f19a7a2dc3e20473405042ceae9a06c4348c5
Harry Raddatz <85199461+PseudoHUEry@users.noreply.github.com>
2022-01-29 17:09:20 -0300

    Initial commit Database

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const startServer = require('./interface')
+const startDatabase = require('./infra/orm/mongoose/database')
+
 
 try{
+startDatabase()
 startServer()
 } catch(e) {
     console.error(e)
